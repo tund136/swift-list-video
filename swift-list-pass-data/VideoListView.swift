55105857e7630a209b31b40c461fd76a21227d54
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-09-06 23:49:56 +0700

    Navigation Link

--- swift-list-pass-data/VideoListView.swift
+++ swift-list-pass-data/VideoListView.swift
@@ -14,24 +14,29 @@ struct VideoListView: View {
     var body: some View {
         NavigationView {
             List(videos, id: \.id) { video in
-                Image(video.imageName)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(height: 70)
-                    .cornerRadius(4.0)
-                    .padding(.vertical, 10)
-                
-                VStack(alignment: .leading, spacing: 5) {
-                    Text(video.title)
-                        .font(.title3)
-                        .fontWeight(.semibold)
-                        .lineLimit(/*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
-                        .minimumScaleFactor(0.5)
-                    
-                    Text(video.uploadDate)
-                        .font(.subheadline)
-                        .foregroundColor(.secondary)
-                }
+                NavigationLink(
+                    destination: VideoDetailView(video: video ),
+                    label: {
+                        Image(video.imageName)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 70)
+                            .cornerRadius(4.0)
+                            .padding(.vertical, 10)
+                        
+                        VStack(alignment: .leading, spacing: 5) {
+                            Text(video.title)
+                                .font(.title3)
+                                .fontWeight(.semibold)
+                                .lineLimit(/*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
+                                .minimumScaleFactor(0.5)
+                            
+                            Text(video.uploadDate)
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                )
             }
             .navigationTitle("Sean's Top 10")
         }
