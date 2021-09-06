f8e78e1f9b529837b054e5717eea891a4e86d5ce
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-09-06 20:52:07 +0700

    Set up project

---
+++ swift-list-pass-data/VideoListView.swift
@@ -0,0 +1,21 @@
+//
+//  VideoListView.swift
+//  swift-list-pass-data
+//
+//  Created by Danh Tu on 06/09/2021.
+//
+
+import SwiftUI
+
+struct VideoListView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoListView()
+    }
+}
---
+++ swift-list-pass-data/swift_list_pass_dataApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_list_pass_dataApp.swift
+//  swift-list-pass-data
+//
+//  Created by Danh Tu on 06/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct swift_list_pass_dataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            VideoListView()
+        }
+    }
+}
